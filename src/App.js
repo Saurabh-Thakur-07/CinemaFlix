43f876984cd196f4de50b3cd02f70bddaf553979
Saurabh-Thakur-07 <saurabhthakurmeerut@gmail.com>
2022-02-03 14:27:49 +0530

    banner navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
 function App() {
   return (
-    <h1>Hello</h1>
+      <>
+        <Navbar />
+        <Banner />
+      </>
   );
 }
 
