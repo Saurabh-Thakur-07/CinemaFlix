db7e9d4041b93fd534bf1007d7ad31607d25d99d
Saurabh-Thakur-07 <saurabhthakurmeerut@gmail.com>
2022-02-10 00:50:10 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
 import Favourites from './Components/Favourites';
+import {BrowserRouter , Route, Switch} from 'react-router-dom';
+
 function App() {
   return (
-      <>
+      <BrowserRouter>
         <Navbar />
+        <Switch>
+            <Route path='/' exact component={Movies}/>
+            <Route path='/favourites' component={Favourites}/>
+        </Switch>
         {/* <Banner />
-        <Movies /> */}
-        <Favourites />
-      </>
+        <Movies />
+        <Favourites /> */}
+      </BrowserRouter>    
   );
 }
 
